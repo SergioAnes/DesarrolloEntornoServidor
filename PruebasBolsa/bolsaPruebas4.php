07166fb0e5629757158d74526329735722553480
SergioAnes <90785004+SergioAnes@users.noreply.github.com>
2021-10-05 10:47:11 +0200

    Update bolsaPruebas4.php

--- PruebasBolsa/bolsaPruebas4.php
+++ PruebasBolsa/bolsaPruebas4.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -16,30 +15,23 @@
 
 
 
-echo "<table border='3' width='400px' style='text-align:center';>";
-echo "<tr>" . "<td>" . "Nombre" . "</td>" . "<td>" . "Precio" . "</td>" . "<td>" . "Variacion" . "</td>" . "<td>" . "Volumen" . "</td>" . "<td>" . "Volumen" . "</td>" . "</tr>"; 
+/*echo "<table border='3' width='400px' style='text-align:center';>";
+echo "<tr>" . "<td>" . "Nombre" . "</td>" . "<td>" . "Precio" . "</td>" . "<td>" . "Variacion" . "</td>" . "<td>" . "Volumen" . "</td>" . "<td>" . "Volumen" . "</td>" . "</tr>"; */
 
 
-$nombre="Empresa ";
+$nombre="E";
 $numeroEmpresa=0;
 $i=0;	
-while ($i<=35) 
 
-
-
-{
-$Ibex35 = array($nombre => array (
+while ($i<=35) {
+$Ibex35 = array($nombre.$numeroEmpresa => array (
 										"Precio" => rand(),
 										"VARIACION(%)" => rand(),
 										"VARIACION(euros)" => rand(),
 										"VOLUMEN" => rand()
 ));
 
-
-
-
-
-	foreach ($Ibex35 as $empresa => $datos) {
+	/*foreach ($Ibex35 as $empresa => $datos) {
 			echo "<tr>" . "<td>" . $empresa . + $numeroEmpresa  . "</td>";
 
 			foreach ($datos as $indice => $valor) {
@@ -47,19 +39,21 @@ $Ibex35 = array($nombre => array (
 					echo  "<td>" . $indice=$valor . "</td>";
 
 			}
-
-
 	}
-
-	echo  "</tr>";
+	echo  "</tr>"; */
 
 	$i++;		
 	$numeroEmpresa++;
 
+	var_dump($Ibex35);
+	print_r($Ibex35);
 }
 
 
-echo "</table>";
+echo "Imprime el valor que tu quieras <br>";
+echo $Ibex35["Empresa"]["Precio"];
+
+/*echo "</table>";*/
 
 //como accedo de forma individual a cada uno de los elementos de ese array. 
 
