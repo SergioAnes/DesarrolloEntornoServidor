e5dd75ed463b61b2097ec711b2f18c038ac93160
SergioAnes <90785004+SergioAnes@users.noreply.github.com>
2021-10-21 01:36:50 +0200

    Add files via upload

---
+++ Forms/ej1-calculadora.php
@@ -0,0 +1,40 @@
+<?php
+
+
+
+echo "<div align='center'>" . "Calculadora" . "</div><br><br>";
+
+
+$numero1 =  $_POST['operando1'];
+$numero2 =  $_POST['operando2'];
+$operacion =  $_POST['operacion'];
+
+
+calculadora($numero1, $numero2, $operacion);
+
+
+function calculadora($operando1, $operando2, $operacion) {
+		echo "<div align='center'>" . "<input type='text' name='operando1' value='$operando1' size=15><br>" . "</div><br><br>";
+	 echo "<div align='center'>" . "El primer numero es " . $operando1 . " y el segundo numero es " . $operando2 . "</div><br><br>";
+
+	switch ($operacion) {
+		case 'Suma':
+			 echo "<div align='center'>" . "Resultado de la operacion: " . $operando1 + $operando2 . "</div>";
+			break;
+		case 'Resta':
+			 echo "<div align='center'>" . "Resultado de la operacion: " . $operando1 - $operando2 . "</div>";
+			break;
+		case 'Divide':
+			 echo "<div align='center'>" . "Resultado de la operacion: " . $operando1 / $operando2 . "</div>";
+			break;
+		case 'Multiplica':
+			 echo "<div align='center'>" . "Resultado de la operacion: " . $operando1 * $operando2 . "</div>";
+			break;		
+		default:
+			echo "Algo ha fallado. Intenta de nuevo";
+			break;
+	}
+}
+
+
+?>
