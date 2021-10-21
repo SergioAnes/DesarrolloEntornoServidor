c90dc5cc0c6c9fc99926d5b05be110b0a7894fe2
SergioAnes <90785004+SergioAnes@users.noreply.github.com>
2021-10-21 10:48:47 +0200

    Update calculadoraPruebaSergio.php

--- Forms/calculadoraPruebaSergio.php
+++ Forms/calculadoraPruebaSergio.php
@@ -1,11 +1,6 @@
 
 <?php
 
-echo "<div align='center'>" . "Calculadora" . "</div><br><br>";
-
-$numero1 =  $_POST['operando1'];
-$numero2 =  $_POST['operando2'];
-$operacion =  $_POST['operacion'];
 
 include("calculadora1.php");
 
