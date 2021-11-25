662f96a7b5742f3e6e54daec14fb58c0ea48847c
SergioAnes <90785004+SergioAnes@users.noreply.github.com>
2021-11-25 10:23:53 +0100

    Add files via upload

---
+++ BBDD/EJERCICIO1/funcionesBBDD.php
@@ -0,0 +1,48 @@
+
+<<?php 
+
+function limpiar_campo($campoformulario) {
+  $campoformulario = trim($campoformulario); //elimina espacios en blanco por izquierda/derecha
+  $campoformulario = stripslashes($campoformulario); //elimina la barra de escape "\", utilizada para escapar caracteres
+  $campoformulario = htmlspecialchars($campoformulario);  
+  return $campoformulario;
+}
+
+function crearConexion () {
+	$servername = "localhost";
+	$username = "root";
+	$password = "rootroot";
+	$dbname="empleados1n";
+	// Create connection
+	$conn = mysqli_connect($servername, $username, $password,$dbname);
+	// Check connection
+	if (!$conn) {
+    die("Ha sucedido un error inexperado en la conexion de la base de datos");
+}
+	return $conn;
+}//fin crear conexion
+
+function altaDepartamento ($Nombredepartamento, $conexion) {
+	$insertarDepartamento = "INSERT INTO departamento (nombre_d) VALUES ('$Nombredepartamento')";
+	if (mysqli_query($conexion, $insertarDepartamento)) {
+    	echo "Se ha creado un nuevo departamento";
+	} else {
+    	echo "Error en el departamento: ";
+	}
+	mysqli_close($conexion);
+}//fin altaDepartamento
+
+
+
+function altaEmpleado ($conexion, $nombreEmpleado, $dni, $fechaNacimiento, $Nombredepartamento) {
+	$insertarEmpleado = "INSERT INTO empleado (dni, nombre, fec_nac, nombre_d) VALUES ('$dni', '$nombreEmpleado', '$fechaNacimiento', '$Nombredepartamento')";
+	if (mysqli_query($conexion, $insertarEmpleado)) {
+    	echo "Se ha creado un nuevo empleado";
+	} else {
+    	echo "Error en la inserción del empleado: ";
+	}
+	mysqli_close($conexion);
+}//fin altaDepartamento
+
+
+ ?>
